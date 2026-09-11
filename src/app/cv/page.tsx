676ff521bc00c7a2cba3c3c7b6ce198de0e19655
hujaifa12345 <hafizjarir@gmail.com>
2026-09-12 00:00:15 +0600

    updated

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -123,7 +123,7 @@ const personalInformation = [
       "Musapur, Bandar, Narayanganj, Dhaka, Bangladesh",
   },
   {
-    label: "Contact Number",
+    label: "Contact Num",
     value: "+880 1883-650010",
   },
 ];
